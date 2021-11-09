cf343aa2f9cefe9946a81c518da9735d80e85ea2
user <promonolit12345@gmail.com>
2021-11-09 18:46:49 +0300

    Task

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -120,6 +120,7 @@ class MainActivity : SongChangeListener, AppCompatActivity() {
             }
         }
 
+        //bar
         playerSeekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 if(p2) {
